bd7a615e776cee36eee060bd5b9b11bade907df3
Jun Zhang <jim.zhang@caicloud.io>
2018-04-23 17:12:15 +0800

    feat: add templates image (#48)
    * feat: add templates image
    
    * chore(makefile): build-linux -> test-linux

---
+++ build/templates/test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Copyright 2018 Caicloud Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Exit on error. Append "|| true" if you expect an error.
+set -o errexit
+# Do not allow use of undefined vars. Use ${VAR:-} to use an undefined VAR
+set -o nounset
+# Catch the error in pipeline.
+set -o pipefail
+
+echo ">>>> Testing build templates"
+
+export OUTPUT_DIR=$(mktemp -d)
+export INPUT_DIR=./templates
+
+build/templates/build.sh
